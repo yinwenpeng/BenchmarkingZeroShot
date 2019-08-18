5c097250632b768a416374d7f9ce9e73a96f8d34
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-18 04:59:44 -0700

    bug fix

--- src/preprocess_situation.py
+++ src/preprocess_situation.py
@@ -1,5 +1,6 @@
 
 import codecs
+from collections import defaultdict
 
 def combine_all_available_labeled_datasets():
     path = '/export/home/Dataset/LORELEI/'
@@ -14,6 +15,7 @@ def combine_all_available_labeled_datasets():
     label_id_re_map = {0:0,1:1, 2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:11,10:9,11:10}
     writefile = codecs.open(path+'sf_all_labeled_data_multilabel.txt', 'w', 'utf-8')
     all_size = 0
+    label2co = defaultdict(int)
     for fil in files:
         print('loading file:', path+fil, '...')
         size = 0
@@ -21,6 +23,8 @@ def combine_all_available_labeled_datasets():
         for line in readfile:
             parts=line.strip().split('\t') #lowercase all tokens, as we guess this is not important for sentiment task
             label_list = parts[1].strip().split()
+            for label in set(label_list):
+                label2co[label]+=1
             text=parts[2].strip()
             writefile.write(' '.join(label_list)+'\t'+text+'\n')
             size+=1
@@ -28,7 +32,7 @@ def combine_all_available_labeled_datasets():
         readfile.close()
         print('size:', size)
     writefile.close()
-    print('all_size:', all_size)
+    print('all_size:', all_size, label2co)
 
 if __name__ == '__main__':
     combine_all_available_labeled_datasets()
