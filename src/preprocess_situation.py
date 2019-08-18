4eb33e22e1f1e6912d53db0675433975b4e1937a
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-18 06:06:32 -0700

    bug fix

--- src/preprocess_situation.py
+++ src/preprocess_situation.py
@@ -34,5 +34,48 @@ def combine_all_available_labeled_datasets():
     writefile.close()
     print('all_size:', all_size, label2co)
 
+
+def split_all_labeleddata_into_subdata_per_label():
+    readfile = codecs.open(path+'sf_all_labeled_data_multilabel.txt', 'r', 'utf-8')
+    label_list = ['search','evac','infra','utils','water','shelter','med','food', 'crimeviolence', 'terrorism', 'regimechange', 'out-of-domain']
+    writefile_list = []
+    for label in label_list:
+        writefile = codecs.open(path+'data_per_label/'+label+'.txt', 'w', 'utf-8')
+        writefile_list.append(writefile)
+    for line in readfile:
+        parts=line.strip().split('\t')
+        label_list_instance = parts[0].strip().split()
+        for label in label_list_instance:
+            writefile_exit = writefile_list[label_list.index(label)]
+            writefile_exit.write(parts[1].strip()+'\n')
+
+    for writefile in writefile_list:
+        writefile.close()
+    readfile.close()
+
+
+
+# def build_zeroshot_test_dev_set():
+#     readfile = codecs.open(path+'sf_all_labeled_data_multilabel.txt', 'r', 'utf-8')
+#     writefile_test = codecs.open(path+'zero-shot-split/test.txt', 'w', 'utf-8')
+#     writefile_dev = codecs.open(path+'zero-shot-split/dev.txt', 'w', 'utf-8')
+#     writefile_remain = codecs.open(path+'unified-dataset-wo-devandtest.txt', 'w', 'utf-8')
+#     label_set = set(['search','evac','infra','utils','water','shelter','med','food', 'crimeviolence', 'terrorism', 'regimechange'])
+#
+#     for line in readfile:
+#         parts=line.strip().split('\t')
+#         label_list = parts[0].strip().split()
+#         if len(label_list) == 1 and label_list[0] == 'out-of-domain':
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    combine_all_available_labeled_datasets()
+    # combine_all_available_labeled_datasets()
+    split_all_labeleddata_into_subdata_per_label()
