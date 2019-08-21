75929a1458f34b0f2bd89f6c29110a68b2566cd3
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-21 05:53:23 -0700

    both unseen and seen use loose

--- src/train_yahoo.py
+++ src/train_yahoo.py
@@ -622,7 +622,7 @@ def main():
     num_train_optimization_steps = None
     if args.do_train:
         # train_examples = processor.get_train_examples_wenpeng('/home/wyin3/Datasets/glue_data/RTE/train.tsv')
-        train_examples, seen_types = processor.get_examples_Yahoo_train('/export/home/Dataset/YahooClassification/yahoo_answers_csv/zero-shot-split/train_pu_half_v0.txt', 1000)
+        train_examples, seen_types = processor.get_examples_Yahoo_train('/export/home/Dataset/YahooClassification/yahoo_answers_csv/zero-shot-split/train_pu_half_v0.txt', 10000)
         # seen_classes=[0,2,4,6,8]
 
         num_train_optimization_steps = int(
