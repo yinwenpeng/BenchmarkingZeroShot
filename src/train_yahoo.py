bd3aa1f216aed0761f2c1f7fcb2e7732c9f53ea3
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-21 11:32:44 -0700

    forfun

--- src/train_yahoo.py
+++ src/train_yahoo.py
@@ -498,7 +498,7 @@ def main():
                         action='store_true',
                         help="Set this flag if you are using an uncased model.")
     parser.add_argument("--train_batch_size",
-                        default=256,
+                        default=128,
                         type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size",
