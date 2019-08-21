145c02dfd38ad03f67196eb1252aaae4a82c32bc
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-21 04:58:46 -0700

    both unseen and seen use loose

--- src/preprocess_yahoo.py
+++ src/preprocess_yahoo.py
@@ -433,7 +433,7 @@ def evaluate_Yahoo_zeroshot_TwpPhasePred(pred_probs, pred_binary_labels_harsh, p
                     if its_prob > max_prob_seen:
                         max_prob_seen = its_prob
                         max_seen_index = j
-            if  max_prob_seen - max_prob_unseen > 0.2:
+            if  max_prob_seen - max_prob_unseen > 0.1:
                 pred_type = eval_hypo_2_type_index[max_seen_index]
             else:
                 pred_type = eval_hypo_2_type_index[max_unseen_index]
