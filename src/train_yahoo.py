c403422a1fe3c32dcd3f18553c51462d63366aec
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-22 04:21:17 -0700

    forfun

--- src/train_yahoo.py
+++ src/train_yahoo.py
@@ -577,7 +577,7 @@ def main():
                         type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size",
-                        default=256,
+                        default=64,
                         type=int,
                         help="Total batch size for eval.")
     parser.add_argument("--learning_rate",
@@ -712,7 +712,7 @@ def main():
     #           num_labels=num_labels)
     # tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=args.do_lower_case)
 
-    pretrain_model_dir = '/export/home/Dataset/fine_tune_Bert_stored/FineTuneOnCombined' #FineTuneOnCombined'# FineTuneOnMNLI
+    pretrain_model_dir = '/export/home/Dataset/fine_tune_Bert_stored/FineTuneOnMNLI' #FineTuneOnCombined'# FineTuneOnMNLI
     model = BertForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=num_labels)
     tokenizer = BertTokenizer.from_pretrained(pretrain_model_dir, do_lower_case=args.do_lower_case)
 
@@ -835,7 +835,7 @@ def main():
                 optimizer.zero_grad()
                 global_step += 1
                 iter_co+=1
-                if iter_co %1000==0:
+                if iter_co %200==0:
                     '''
                     start evaluate on dev set after this epoch
                     '''
