754d8c854f805bebc461927a4ba30cc90146f573
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-26 13:10:42 -0700

    try different

--- src/test_emotion_agnostic.py
+++ src/test_emotion_agnostic.py
@@ -710,7 +710,7 @@ def main():
     #           num_labels=num_labels)
     # tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=args.do_lower_case)
 
-    pretrain_model_dir = '/export/home/Dataset/fine_tune_Bert_stored/FineTuneOnFEVER' #FineTuneOnCombined'# FineTuneOnMNLI
+    pretrain_model_dir = '/export/home/Dataset/fine_tune_Bert_stored/FineTuneOnRTE' #FineTuneOnCombined'# FineTuneOnMNLI
     model = BertForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=num_labels)
     tokenizer = BertTokenizer.from_pretrained(pretrain_model_dir, do_lower_case=args.do_lower_case)
 
