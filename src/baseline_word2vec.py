f718ff4fd784d7dad34682b370a99818201a9886
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-27 11:23:15 -0700

    try different

--- src/baseline_word2vec.py
+++ src/baseline_word2vec.py
@@ -22,64 +22,6 @@ def baseline_w2v():
 
 
 
-    '''yahoo'''
-    type_list = ['Society & Culture', 'Science & Mathematics', 'Health', 'Education & Reference','Computers & Internet','Sports','Business & Finance','Entertainment & Music','Family & Relationships','Politics & Government']
-    type_2_emb = []
-    for type in type_list:
-        type_2_emb.append(sent_2_emb(type.split()))
-    readfile = codecs.open('/export/home/Dataset/YahooClassification/yahoo_answers_csv/zero-shot-split/test.txt', 'r', 'utf-8')
-    gold_label_list = []
-    pred_label_list = []
-    co = 0
-    for line in readfile:
-        parts = line.strip().split('\t')
-        gold_label_list.append(parts[0])
-        text = parts[1].strip()
-        max_cos = 0.0
-        max_type = ''
-        for i, type in enumerate(type_list):
-            text_emb = sent_2_emb(text.split())
-            type_emb = type_2_emb[i]
-            cos = 1.0-cosine(text_emb, type_emb)
-            if cos > max_cos:
-                max_cos = cos
-                max_type = str(i)
-        pred_label_list.append(max_type)
-        co+=1
-        if co % 100 == 0:
-            print('yahoo co:', co)
-    readfile.close()
-
-    # all_test_labels = list(set(gold_label_list))
-    # f1_score_per_type = f1_score(gold_label_list, pred_label_list, labels = all_test_labels, average=None)
-    seen_types_group = [['0','2','4','6','8'],['1','3','5','7','9']]
-
-    for i in range(len(seen_types_group)):
-        seen_types = set(seen_types_group[i])
-
-        seen_hit = 0.0
-        seen_size = 0
-        unseen_hit = 0.0
-        unseen_size = 0
-        for i in range(len(gold_label_list)):
-            if gold_label_list[i] in seen_types:
-
-                seen_size+=1
-                if gold_label_list[i] == pred_label_list[i]:
-                    seen_hit+=1
-            else:
-                unseen_size+=1
-                if gold_label_list[i] == pred_label_list[i]:
-                    unseen_hit+=1
-        print('seen:', seen_hit/seen_size, 'unseen:', unseen_hit/unseen_size)
-
-    all_hit = 0
-    for i in range(len(gold_label_list)):
-        if gold_label_list[i] == pred_label_list[i]:
-            all_hit+=1
-
-
-    print('overall:', all_hit/len(gold_label_list))
 
 
     '''emotion'''
@@ -191,6 +133,64 @@ def baseline_w2v():
 
 
 
+    '''yahoo'''
+    type_list = ['Society & Culture', 'Science & Mathematics', 'Health', 'Education & Reference','Computers & Internet','Sports','Business & Finance','Entertainment & Music','Family & Relationships','Politics & Government']
+    type_2_emb = []
+    for type in type_list:
+        type_2_emb.append(sent_2_emb(type.split()))
+    readfile = codecs.open('/export/home/Dataset/YahooClassification/yahoo_answers_csv/zero-shot-split/test.txt', 'r', 'utf-8')
+    gold_label_list = []
+    pred_label_list = []
+    co = 0
+    for line in readfile:
+        parts = line.strip().split('\t')
+        gold_label_list.append(parts[0])
+        text = parts[1].strip()
+        max_cos = 0.0
+        max_type = ''
+        for i, type in enumerate(type_list):
+            text_emb = sent_2_emb(text.split())
+            type_emb = type_2_emb[i]
+            cos = 1.0-cosine(text_emb, type_emb)
+            if cos > max_cos:
+                max_cos = cos
+                max_type = str(i)
+        pred_label_list.append(max_type)
+        co+=1
+        if co % 100 == 0:
+            print('yahoo co:', co)
+    readfile.close()
+
+    # all_test_labels = list(set(gold_label_list))
+    # f1_score_per_type = f1_score(gold_label_list, pred_label_list, labels = all_test_labels, average=None)
+    seen_types_group = [['0','2','4','6','8'],['1','3','5','7','9']]
+
+    for i in range(len(seen_types_group)):
+        seen_types = set(seen_types_group[i])
+
+        seen_hit = 0.0
+        seen_size = 0
+        unseen_hit = 0.0
+        unseen_size = 0
+        for i in range(len(gold_label_list)):
+            if gold_label_list[i] in seen_types:
+
+                seen_size+=1
+                if gold_label_list[i] == pred_label_list[i]:
+                    seen_hit+=1
+            else:
+                unseen_size+=1
+                if gold_label_list[i] == pred_label_list[i]:
+                    unseen_hit+=1
+        print('seen:', seen_hit/seen_size, 'unseen:', unseen_hit/unseen_size)
+
+    all_hit = 0
+    for i in range(len(gold_label_list)):
+        if gold_label_list[i] == pred_label_list[i]:
+            all_hit+=1
+
+
+    print('overall:', all_hit/len(gold_label_list))
 
 if __name__ == '__main__':
 
