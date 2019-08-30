f013059cbb10683e74f8c209044a3fd8b23f428a
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-30 04:49:55 -0700

    first runable ESA for emotion

--- src/baseline_wiki_based_yahoo.py
+++ src/baseline_wiki_based_yahoo.py
@@ -825,7 +825,7 @@ def main():
                 optimizer.zero_grad()
                 global_step += 1
                 iter_co+=1
-                if iter_co %400==0:
+                if iter_co %5==0:
                     '''
                     start evaluate on dev set after this epoch
                     '''
