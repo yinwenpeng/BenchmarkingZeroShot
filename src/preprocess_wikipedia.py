a6dd8016bbaf601e470d6dfe8f5a01ba736d9792
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-30 03:54:06 -0700

    first runable ESA for emotion

--- src/preprocess_wikipedia.py
+++ src/preprocess_wikipedia.py
@@ -1,10 +1,21 @@
 from wikipedia import WikipediaPage
+import json
+
+def build_wiki_category_dataset():
+    readfile = codecs.open('/export/home/Dataset/wikipedia/parsed_output/tokenized_wiki/tokenized_wiki.txt', 'r', 'utf-8')
+    writefile = codecs.open('/export/home/Dataset/wikipedia/parsed_output/tokenized_wiki/tokenized_wiki2categories.txt', 'w', 'utf-8')
+    for line in f:
+        try:
+            line_dic = json.loads(line)
+        except ValueError:
+            continue
+        # title = line_dic.get('title')
+        title_id = title2id.get('id')
+        article = WikipediaPage(pageid=title_id)
+        type_list = article.categories
+        print(type_list)
+        exit(0)
 
 
-
-article = WikipediaPage(pageid='39526183', preload=True)
-print(article.categories)
-sections = article.sections()
-print('sections titles:', sections)
-for sec in sections:
-    print(sec, '-->', article.section(sec))
+if __name__ == '__main__':
+    build_wiki_category_dataset()
