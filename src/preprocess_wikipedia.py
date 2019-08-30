649579ae37e166bc40aaf850048d464e5e49721a
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2019-08-30 03:37:21 -0700

    first runable ESA for emotion

--- src/preprocess_wikipedia.py
+++ src/preprocess_wikipedia.py
@@ -4,3 +4,7 @@ from wikipedia import WikipediaPage
 
 article = WikipediaPage(pageid='39526180')
 print(article.categories)
+sections = article.sections
+print('sections titles:', sections)
+for sec in sections:
+    print(sec, '-->', article.section(sec))
